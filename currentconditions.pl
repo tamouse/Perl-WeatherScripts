ff0781676646a15b5e979676480c10281e4a1803
Tamara Temple <tamouse@gmail.com>
2011-11-20 14:05:38 -0600

    Add POD

--- currentconditions.pl
+++ currentconditions.pl
@@ -1,13 +1,31 @@
 #!/usr/bin/perl -w
 #
-# currentconditions
+# currentconditions - display the current weather conditions for a
+#                     given city from the Google Weather API 
 #
 # Author: Tamara Temple <tamara@tamaratemple.com>
 # Created: 2011/11/19
-# Time-stamp: <2011-11-19 19:54:57 tamara>
+# Time-stamp: <2011-11-20 14:04:02 tamara>
 # Copyright (c) 2011 Tamara Temple Web Development
 # License: GPLv3
 #
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# See [Google Weather API Wiki Article](http://wiki.tamaratemple.com/Main/GoogleWeatherApi)
+#
+
+
 
 use strict;
 use XML::Simple;
@@ -71,5 +89,84 @@ Sky: %s
        $w->{weather}->{current_conditions}->{condition}->{data}
     );
 
-       
-       
+=pod
+
+=head1 NAME
+
+currentconditions.pl
+
+=head1 SYNOPSIS
+
+currentconditions [-c <city>] [-d <domain>] [-g <geoip-database>]
+
+=head1 DECSRIPTION
+
+Retrieves the current weather conditions from the (undocumentted)
+Google Weather API service for the given city, or if omitted, will try
+to determine the current location based upon the network's external IP
+address.
+
+=head1 OPTIONS
+
+=over
+
+=item *
+-d|--domain-name [<domain>]
+Domain name to use for weather information (defaults to tamnet.homeip.net)
+
+=item *
+-g|--geoip-data [<file>]
+GeoIP City database file (defaults to /usr/share/GeoIP/GeoLiteCity.dat)
+
+=item *
+-c|--city [<city>]
+City to look up weather for (defaults to )
+
+=back
+
+=head1 FILES
+
+If no city is given, the external IP is used to look up the city based
+upon the GeoLiteCity data base avaialable from
+L<http://www.maxmind.com/app/ip-location>. The default location is
+C</usr/share/GeoIP/GeoLiteCity.dat>.
+
+=head1 EXAMPLES
+
+=over
+
+=item C<currentconditions.pl>
+
+=back
+
+Shows current weather conditions for the city based up on the external
+IP address.
+
+=over
+
+=item C<currentconditions -c"St. Paul, MN">
+
+=item C<currentconditions --city="St. Paul, MN">
+
+=back
+
+Shows current weather conditoins for St. Paul, Minnesota.
+
+=over
+
+=item C<currentconditions -d"tamnet.homeip.net">
+
+=item C<currentconditions --domain-name=tamnet.homeip.net>
+
+=back
+
+Shows current weather conditions for the location of the domain
+tamnet.homeip.net.
+
+=head1 SEE ALSO
+
+L<http://wiki.tamaratemple.com/Main/GoogleWeatherApi>
+
+=head1 AUTHOR
+
+Tamara Temple <tamara@tamaratemple.com>
